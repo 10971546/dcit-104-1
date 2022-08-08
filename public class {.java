080d724908c07de3035c35ab076445023db5360c
joseph nmaawie beresi 10971546 <jnberesie@st.ug.edu.gh>
2022-08-08 02:47:41 -0700

    my first commit

---
+++ public class {.java
@@ -0,0 +1,12 @@
+public class {
+    public static void main(String args[]){
+
+      int array[]=new int[501];
+      for(int a=0;a<array.length;a++){
+        int array[a]=(a*2); sum+=array[a];
+      }
+      int b=sum/500
+       System.out.println(b);
+
+    }
+}
